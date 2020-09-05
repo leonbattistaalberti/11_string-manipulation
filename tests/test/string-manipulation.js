1fc7f371a197b68405b08eb1b32b0b380beb15fb
thelonehegelian <thelonehegelian@gmail.com>
2020-09-05 12:57:36 +0100

    Pass return string lengt

--- tests/test/string-manipulation.js
+++ tests/test/string-manipulation.js
@@ -5,4 +5,9 @@ contract("Strings", () => {
 	before(async () => {
 		strings = await Strings.deployed();
 	});
+
+	it("Should provide string length", async () => {
+		const str = await strings.length("hello");
+		assert.equal(str, 5);
+	});
 });
