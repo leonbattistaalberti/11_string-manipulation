e96739ffbf0e7a927b7af0837f1383a6718a5d3f
thelonehegelian <thelonehegelian@gmail.com>
2020-09-05 13:02:27 +0100

    Pass concatenate 2 strings

--- tests/test/string-manipulation.js
+++ tests/test/string-manipulation.js
@@ -10,4 +10,13 @@ contract("Strings", () => {
 		const str = await strings.length("hello");
 		assert.equal(str, 5);
 	});
+
+	it("Should concatenate 2 strings", async () => {
+		const str_1 = "Hi";
+		const str_2 = "_Everybody";
+
+		const concatString = await strings.concatenate(str_1, str_2);
+
+		assert.equal(concatString, `${str_1}${str_2}`);
+	});
 });
